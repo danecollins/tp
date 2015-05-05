46e6057e95c1463737442ee186e6993c8033e2a7
Dane Collins <dane@pobox.com>
2015-05-04 19:22:12 -0700

    Add city to place navigation pages

--- places/urls.py
+++ places/urls.py
@@ -3,4 +3,7 @@ from places import views
 
 urlpatterns = patterns('',
                        url(r'^$', views.index, name='index'),
+                       url(r'^city/$', views.city_list, name='city_list'),
+                       url(r'^city/(?P<city>[\w|\W]+)/(?P<locale>[\w|\W]+)/$', views.place_list, name='place_list'),
+                       url(r'^city/(?P<city>[\w|\W]+)/$', views.locale_list, name='locale_list'),
                        )
--- places/views.py
+++ places/views.py
@@ -6,3 +6,21 @@ from places.models import Place
 
 def index(request):
     return render(request, 'places/index.html')
+
+
+def city_list(request):
+    cities = sorted(set([x.city for x in Place.objects.all()]))
+    return render(request, 'places/city_list.html', {'clist': cities})
+
+
+def locale_list(request, city):
+    locales = sorted(set([x.locale for x in Place.objects.filter(city=city)]))
+    return render(request, 'places/locale_list.html', {'llist': locales,
+                                                       'city': city})
+
+
+def place_list(request, city, locale):
+    locales = sorted(set([x.name for x in Place.objects.filter(city=city, locale=locale)]))
+    return render(request, 'places/place_list.html', {'plist': locales,
+                                                      'city': city,
+                                                      'locale': locale})

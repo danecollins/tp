4759f1375c2a37633ccc7e510849bb962dd17adb
Dane Collins <dane@pobox.com>
2015-05-04 17:58:48 -0700

    add index page and style sheets with base menu

--- places/views.py
+++ places/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from places.models import Place
 
 # Create your views here.
+
+
+def index(request):
+    return render(request, 'places/index.html')
--- tp/urls.py
+++ tp/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
-urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'tp.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
+from places import views
 
-    url(r'^admin/', include(admin.site.urls)),
-)
+urlpatterns = patterns('',
+                       url(r'^$', views.index, name='index'),
+                       url(r'^places/', include('places.urls',
+                                                namespace="places")),
+                       url(r'^admin/', include(admin.site.urls)),
+                       )

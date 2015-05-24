b6da8fd46810338a42a4316db067827cfa2ceb01
Dane Collins <dane@pobox.com>
2015-05-23 21:36:59 -0700

    Create script that allows tests to be run locally or on heroku

--- tests/test_dbdata.py
+++ tests/test_dbdata.py
@@ -15,9 +15,12 @@ class TestDataIntegrity(unittest.TestCase):
     def test_for_trailing_blanks(self):
         places = Place.objects.all()
         for p in places:
-            self.assertEqual(p.name, p.name.strip())
-            self.assertEqual(p.locale, p.locale.strip())
-            self.assertEqual(p.city, p.city.strip())
+            self.assertEqual(p.name, p.name.strip(),
+                             msg='name {} contains spaces'.format(p.name))
+            self.assertEqual(p.locale, p.locale.strip(),
+                             msg='locale {} on place {} has spaces'.format(p.locale, p.name))
+            self.assertEqual(p.city, p.city.strip(),
+                             msg='city {} on place {} has spaces'.format(p.city, p.name))
 
     def test_yelp_links(self):
         places = Place.objects.all()
@@ -28,7 +31,8 @@ class TestDataIntegrity(unittest.TestCase):
     def test_rating_range(self):
         places = Place.objects.all()
         for p in places:
-            self.assertTrue(p.rating >= -1 and p.rating <= 3)
+            self.assertTrue(p.rating >= 0 and p.rating <= 3,
+                            msg='rating {} out of range on place {}'.format(p.rating, p.name))
 
 if __name__ == '__main__':
     unittest.main()

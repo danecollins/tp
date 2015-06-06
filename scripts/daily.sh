15b7a104c6c41254b3f71ed2b5777d51e6995f23
Dane Collins <dane@pobox.com>
2015-06-05 18:08:59 -0700

    change dbpull to go to tpdata.heroku

--- scripts/daily.sh
+++ scripts/daily.sh
@@ -1,27 +1,29 @@
 #!/bin/bash -l
 
 cd ~/src/tp
-echo -n "*** " >> daily.log
-date >> daily.log
+echo -n "*** " >> ~/daily.log
+date >> ~/daily.log
 source ~/env/dj/bin/activate
-dropdb tpdata >> daily.log 2>&1
+dropdb tpdata.heroku >> ~/daily.log 2>&1
 if [ $? -ne 0 ]; then
-    echo "ERROR: could not drop database" 
-    echo "ERROR: could not drop database" >> daily.log
+ 0   echo "ERROR: could not drop database" 
+    echo "ERROR: could not drop database" >> ~/daily.log
     exit 1
 fi
 
-heroku pg:pull HEROKU_POSTGRESQL_MAUVE tpdata --app trackplaces >> daily.log 2>&1
+heroku pg:pull HEROKU_POSTGRESQL_MAUVE tpdata.heroku --app trackplaces >> ~/daily.log 2>&1
 if [ $? -ne 0 ]; then
     echo "ERROR: heroku pull failed"
-    echo "ERROR: heroku pull failed" >> daily.log
+    echo "ERROR: heroku pull failed" >> ~/daily.log
     exit 1
 fi
 
-echo "INFO: heroku pull completed" >> daily.log
-scripts/dbsummary.py >> daily.log 2>&1
+echo "INFO: heroku pull completed" >> ~/daily.log
+export DATABASE_URL=postgres:///tpdata.heroku
+scripts/dbsummary.py >> ~/daily.log 2>&1
+#curl http://trackplaces.heroku.com/watch/checkin/JHUNHHH5
 
-echo "INFO: next backup ladera database" >> daily.log
+echo "INFO: next backup ladera database" >> ~/daily.log
 cd ~/src/scripts
-./backup_ladera_prod.sh >> daily.log 2>&1
+./backup_ladera_prod.sh >> ~/daily.log 2>&1
 
